863795da34f29eb6c8c2c7d6ca283b50c38c848f
ChingFun <eamil@ching.fun>
2022-02-21 17:34:55 +0800

    second commit

--- src/fun/it/java/GitTest.java
+++ src/fun/it/java/GitTest.java
@@ -7,5 +7,10 @@ package fun.it.java;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git");
+        System.out.println("hello Git");
+        System.out.println("hello Git");
+        System.out.println("hello Git");
+        System.out.println("hello Git");
+        System.out.println("hello Git");
     }
 }
