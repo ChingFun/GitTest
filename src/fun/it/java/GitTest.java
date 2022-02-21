6cab2d8716ce39c250cbfedb9a5ebc00492c0e99
ChingFun <eamil@ching.fun>
2022-02-21 21:36:08 +0800

    final commit

--- src/fun/it/java/GitTest.java
+++ src/fun/it/java/GitTest.java
@@ -6,5 +6,8 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("master branch");
         System.out.println("hot-fix branch");
+        System.out.println("hot-fix branch");
+        System.out.println("hot-fix branch");
+        System.out.println("hot-fix branch");
     }
 }
