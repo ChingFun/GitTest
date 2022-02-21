062f74b00a564ff87a679147d3a5b23ea92874c3
ChingFun <eamil@ching.fun>
2022-02-21 17:33:53 +0800

    first commit

---
+++ src/fun/it/java/GitTest.java
@@ -0,0 +1,11 @@
+package fun.it.java;
+
+/**
+ * @author li
+ * @create 2022-02-21 17:31
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+    }
+}
