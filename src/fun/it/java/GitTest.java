b58401ce8d43bf3bc83883bc84ba599770408763
ChingFun <eamil@ching.fun>
2022-02-21 21:04:59 +0800

    master init

--- src/fun/it/java/GitTest.java
+++ src/fun/it/java/GitTest.java
@@ -1,13 +1,9 @@
 package fun.it.java;
 
 /**
- * @author li
- * @create 2022-02-21 17:31
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Git Hello");
-        System.out.println("hello Git");
-        System.out.println("Heo");
+        System.out.println("hot-fix branch");
     }
 }
